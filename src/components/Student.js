05839f1a16c2de22c4bd780ff638afdf37da4702
Amel <amelntamark@gmail.com>
2022-06-17 17:18:44 -0700

    add ability to update state of Student component

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import './Student.css';
 
 const Student = (props) => {
   const name = props.name;
   const email = props.email;
+
+  const [isPresent, setIsPresent] = useState(false);
+
+  const togglePresence = () => {
+    setIsPresent(!isPresent)
+  }
+  
+  const nameColor = isPresent ? 'green' : 'red';
+
     return (
+      <React.Fragment>
         <ul>
-            <li>Nickname: {name}</li>
+            <li className={nameColor}>Nickname: {name}</li>
             <li>Email: {email}</li>
         </ul>
+        <button onClick={togglePresence}>Toggle if {props.name} is present</button>
+      </React.Fragment>
     )
 }
 
